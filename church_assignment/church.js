70d2a9877b22a363d9433cbb68b6e7fbfe3e2260
AbouCloud <Abouzari6839@gmail.com>
2025-09-15 18:53:20 +0000

    church web site

---
+++ church_assignment/church.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+});
+// venobox
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane'
+});
